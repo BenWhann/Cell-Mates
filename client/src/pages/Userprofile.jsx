6ba38d1b8539e9d79ad1ffd3c1ad44e781d4b70b
BenWhann <whann004@umn.edu>
2023-10-10 21:04:54 -0500

    userprofile formatting

--- client/src/pages/Userprofile.jsx
+++ client/src/pages/Userprofile.jsx
@@ -3,28 +3,28 @@ import '../styles/Userprofile.css'
 export default function userProfilePage() {
 
     return (
-        <div id='userProfile'>
-            <div id='profileHeader'>
+        <div id='userProfile' className='container d-flex flex-column align-items-center'>
+            <div id='profileHeader' className='m-3 d-flex flex-column justify-content-center'>
                 <h1>User Profile</h1>
                 <button id='editBtn'>Edit</button>
             </div>
-            <div id='basicInfo'>
+            <div id='basicInfo' className='m-3 d-flex flex-column align-items-center'>
                 <h2>Basic Information</h2>
                 <div>Name</div>
                 <div>Sex</div>
                 <div>Location</div>
                 <div>Profile Picture</div>
             </div>
-            <div id='prisonerDetails'>
+            <div id='prisonerDetails' className='m-3 d-flex flex-column align-items-center'>
                 <h2>Prisoner Details</h2>
                 <div>Prison Address</div>
                 <div>Commissary Item Wish List</div>
             </div>
-            <div id='communicationSettings'>
+            <div id='communicationSettings' className='m-3 d-flex flex-column align-items-center'>
                 <h2>Communication Settings</h2>
                 <div>Allow Email Communication?</div>
             </div>
-            <div>
+            <div id='civilianPreferences' className='m-3 d-flex flex-column align-items-center'>
                 <h2>Civilian Preferences</h2>
                 <div>Sentence Length</div>
                 <div>Deal Breaker Crimes</div>
