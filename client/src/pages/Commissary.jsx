a6075183abfb890b163db95b6a271a223457a2f0
BenWhann <whann004@umn.edu>
2023-10-10 20:43:29 -0500

    user profile html

--- client/src/pages/Commissary.jsx
+++ client/src/pages/Commissary.jsx
@@ -4,7 +4,7 @@ export default function CommisaryPage() {
 
     return (
         <div>
-            
+
         </div>
     );
 
--- client/src/pages/Userprofile.jsx
+++ client/src/pages/Userprofile.jsx
@@ -3,8 +3,32 @@ import '../styles/Userprofile.css'
 export default function userProfilePage() {
 
     return (
-        <div>
-            
+        <div id='userProfile'>
+            <div id='profileHeader'>
+                <h1>User Profile</h1>
+                <button id='editBtn'>Edit</button>
+            </div>
+            <div id='basicInfo'>
+                <h2>Basic Information</h2>
+                <div>Name</div>
+                <div>Sex</div>
+                <div>Location</div>
+                <div>Profile Picture</div>
+            </div>
+            <div id='prisonerDetails'>
+                <h2>Prisoner Details</h2>
+                <div>Prison Address</div>
+                <div>Commissary Item Wish List</div>
+            </div>
+            <div id='communicationSettings'>
+                <h2>Communication Settings</h2>
+                <div>Allow Email Communication?</div>
+            </div>
+            <div>
+                <h2>Civilian Preferences</h2>
+                <div>Sentence Length</div>
+                <div>Deal Breaker Crimes</div>
+            </div>
         </div>
     )
 
